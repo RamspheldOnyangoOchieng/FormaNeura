4c30f9fb10f0a022962221dc5909136d58fc75b2
RamspheldOnyangoOchieng <ramspheldonyango@gmail.com>
2025-05-07 16:51:41 +0300

    get_form_by_id added to the supabase_client.py

--- app/db/supabase_client.py
+++ app/db/supabase_client.py
@@ -10,3 +10,8 @@ def store_form_request(user, form_link):
     client = supabase.create_client(current_app.config['SUPABASE_URL'], current_app.config['SUPABASE_KEY'])
     response = client.table('form_requests').insert({"user": user, "form_link": form_link}).execute()
     return response
+
+def get_form_by_id(form_id):
+    client = supabase.create_client(current_app.config['SUPABASE_URL'], current_app.config['SUPABASE_KEY'])
+    response = client.table('forms').select("*").eq("id", form_id).single().execute()
+    return response.data
