95b98e05516c67afa0781ff02df1bfc9d555ba79
RamspheldOnyangoOchieng <ramspheldonyango@gmail.com>
2025-05-08 09:08:54 +0300

    Updated the requirements.txt

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 from app import db
-from app import routes, models, forms
+from app import routes, forms
 app = create_app()
 
 if __name__ == '__main__':
